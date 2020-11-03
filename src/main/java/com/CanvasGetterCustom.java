687646a14238f0c70cb07bbe262317b4b8587db2
dev-segal <ysegal7@student.touro.edu>
2020-11-03 09:59:18 -0500

    Revert "Merge branch 'experimental'"
    This reverts commit 47af58ee6608804d4db6b7f3c54c62a79b5e039d, reversing
    changes made to ccd1bbfa8a713995a3ac426f7b1fdd3f20b78317.

--- src/main/java/com/CanvasGetterCustom.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-/** represents canvas features that we have to integrate ourselves (using the canvas api)
- * instead of the KSU Canvas API, because some features just do not seem possible (or correct) in the current KSU API.
-**/
-public class CanvasGetterCustom {
-    
-}

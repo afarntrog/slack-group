1d50c79d6dc8a2d716dee1b78b59a9ec91599862
dev-segal <ysegal7@student.touro.edu>
2020-11-03 09:20:16 -0500

    A solution to some of the problems of the KSU api.

---
+++ src/main/java/com/CanvasGetterCustom.java
@@ -0,0 +1,8 @@
+package com;
+
+/** represents canvas features that we have to integrate ourselves (using the canvas api)
+ * instead of the KSU Canvas API, because some features just do not seem possible (or correct) in the current KSU API.
+**/
+public class CanvasGetterCustom {
+    
+}

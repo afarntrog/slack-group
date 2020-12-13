084a8025484da44aded3cd93ed77cfdd12e94912
dev-segal <ysegal7@student.touro.edu>
2020-12-13 11:57:09 -0500

    Fixed assertion statements

--- src/test/java/com/TestMain.java
+++ src/test/java/com/TestMain.java
@@ -15,25 +15,25 @@ public class TestMain {
     @Test
     public void testSaveUserInformationUsesCorrectResponseForSuccessfulOperation() {
         Main.saveUserInformation("U01DN1LCYEA", "10898~59dEJ3WL5f25MgGgDemzcQvvtzG23b8sDcl9stx2bI1K9Y9HzJ1WgOP21rmup0ag", new SlashCommandContext());
-        assertEquals(Main.response, ":heavy_check_mark: We have successfully saved your token!");
+        assertEquals(":heavy_check_mark: We have successfully saved your token!", Main.response);
     }
 
     @Test 
     public void testSaveUserInformationUsesCorrectResponseForUnsuccessfulOperation() {
         Main.saveUserInformation("U01DN1LCYEA", "10898~59dEJ3WL5f25MgGgDemzcQvvtzG23b8sDcl9stx2bI1K9Y9HzJ1WgOP21rmup0ag", new SlashCommandContext());
-        assertEquals(Main.response, "hmm.. we're having trouble saving your token. \n\nWe'll notify our developers immediately");
+        assertEquals("hmm.. we're having trouble saving your token. \n\nWe'll notify our developers immediately", Main.response);
     }
 
     @Test 
     public void testGetCanvasTokenFromUserIdReturnsEmptyStringForIncorrectUserId() {
         String canvasToken = Main.getCanvasTokenFromUserId("wrong user id");
-        assertEquals(canvasToken, "");
+        assertEquals("", canvasToken);
     }
 
     @Test 
     public void testGetCanvasTokenFromUserIdReturnsCorrectTokenForCorrectUserId() {
         String canvasToken = Main.getCanvasTokenFromUserId("U01DN1LCYEA");
-        assertEquals(canvasToken, "10898~59dEJ3WL5f25MgGgDemzcQvvtzG23b8sDcl9stx2bI1K9Y9HzJ1WgOP21rmup0ag");
+        assertEquals("10898~59dEJ3WL5f25MgGgDemzcQvvtzG23b8sDcl9stx2bI1K9Y9HzJ1WgOP21rmup0ag", canvasToken);
     }
 
     @Test 

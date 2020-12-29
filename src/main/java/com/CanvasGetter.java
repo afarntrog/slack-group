4f5062c1c61b6abbecb17c1425c76452891f0906
AaronF <aaronfarntrog@gmail.com>
2020-12-28 21:38:16 -0500

    fix

--- src/main/java/com/CanvasGetter.java
+++ src/main/java/com/CanvasGetter.java
@@ -118,6 +118,7 @@ public class CanvasGetter {
         
         for(Course course : myCourses) {
             LOG.info("  " + course.getName());
+            if (course == null) continue;
 
             assignments = getAssignments(course);
             int i = 1;
